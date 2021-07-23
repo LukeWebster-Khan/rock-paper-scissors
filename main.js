2d20d9c26e9969e17812cee3fed88078962a2461
Luke <lukewebsterkhan@gmail.com>
2021-07-23 23:33:18 +0100

    modified

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 /* rock-paper-scissors */
 function playRound(playerSelection, computerSelection){
-
 }
 const playerSelection = "rock";
 const computerSelection = computerPlay();
 console.log(playRound(playerSelection, computerSelection));
+
+function game(playRound)
