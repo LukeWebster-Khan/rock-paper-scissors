5ddf5a68f93c5174759522c7e96b29744fd6491e
Luke <lukewebsterkhan@gmail.com>
2021-07-23 23:21:05 +0100

    added playRound function

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
 /* rock-paper-scissors */
+function playRound(playerSelection, computerSelection){
+
+}
+const playerSelection = "rock";
+const computerSelection = computerPlay();
+console.log(playRound(playerSelection, computerSelection));
