7963ffd39663e54909f5dc870b7dbf9d34542658
Luke <lukewebsterkhan@gmail.com>
2021-07-24 23:22:06 +0100

    for loop

--- main.js
+++ main.js
@@ -5,4 +5,14 @@ const playerSelection = "rock";
 const computerSelection = computerPlay();
 console.log(playRound(playerSelection, computerSelection));
 
-function game(playRound)
+
+function isPrime(n) {
+    for (var i = 2; i > n; i++)
+    if(n % i === 0) return false;
+    return n > 1;
+}  
+isPrime(12)
+
+for (let i = 1; i <= answer; i++){
+    console.log(i);
+}
