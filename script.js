98365279fec08602fbf6209f704b4fa0aa800b7c
Luke <lukewebsterkhan@gmail.com>
2021-07-28 21:59:14 +0100

    imported emojis in html/css

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
 const selectionButtons = document.querySelectorAll('[data-selection]')
+const finalColumn = document.querySelector('[data-final-column')
 const SELECTIONS = [ 
     {
         name: 'rock',
+        emoji: '👊',
         beats: 'scissors'
     },
     {
         name: 'paper',
+        emoji: '✋',
         beats: 'rock'
     },
     {
         name: 'scissors',
+        emoji: '✂️',
         beats: 'paper'
     }
 ]
@@ -23,9 +27,27 @@ selectionButtons.forEach(selectionButton => {
 })
 
 function makeSelection(selection){
-    console.log(selection)
+    const computerSelection = randomSelection()
+    const youreWinner = isWinner(selection, computerSelection)
+    const computerWinner = isWinner(computerSelection, selection)
+  
+    addSelectionResult(computerSelection, computerWinner)
+    addSelectionResult(selection, youreWinner)
+}
+
+function addSelectionResult(selection, winner) {
+    const div = document.createElement('div')
+    div.innerText = selection.emoji
+    div.classList.add('result-selection')
+    if (winner) div.classList.add('winner')
+    finalColumn.after(div)
+}
+
+function isWinner(selection, opponentSelection){
+    return selection.beats === opponentSelection.name
 }
 
 function randomSelection() {
-    const randomIndex = Math.random() * SELECTIONS.length
+    const randomIndex = Math.floor(Math.random() * SELECTIONS.length)
+    return SELECTIONS[randomIndex]
 }
